5d81406b955f35e68b9a314904e2df55d77ff3df
ScottMastro <scott-mastro@hotmail.com>
2025-09-05 12:26:18 -0400

    transfer between devices

--- pangyplot/static/js/graph/engines/path-highlight/animation/animation-tick.js
+++ pangyplot/static/js/graph/engines/path-highlight/animation/animation-tick.js
@@ -1,17 +1,27 @@
-import { tickAnimation, pauseAnimation } from './animation-state.js';
+import { tickAnimation, pauseAnimation, resetAnimation } from './animation-state.js';
 import { getNodeElements } from '../../../graph-data/graph-manager.js';
 import { updateStepDisplay } from '../ui/path-highlight-ui.js';
 
 const animatedNodes = [];
+var animationBubblePath = null;
 var animationPath = null;
 var currentStep = -1;
+var stepStructure = [];
 
 export function setAnimationPath(path){
     animationPath = path.bubble_path;
+    animationBubblePath = path.bubble_path;
     resetAnimation();
+    currentStep = -1;
+    stepStructure = [0];
+    updateStepDisplay(null);
 }
 
-function addPathStep(forceGraph, nodeid, direction) {
+function updatePathStep(tick) {
+
+    //get next step
+    currentStep += tick;
+
     console.log("Adding path step:", nodeid, direction);
 
     animatedNodes.push(nodeid);
@@ -25,40 +35,23 @@ function addPathStep(forceGraph, nodeid, direction) {
         }
     });
 
+
+        console.log("Current step:", currentStep, animationPath[currentStep-1]);
+    
+
+
+    return null;//splitStep(animationPath[currentStep]);
 }
 
 export function pathHighlightTick(forceGraph) {
-    if (!isAnimationPlaying()) return
-
-    frameCount++;
-    if (frameCount >= framesPerStep) {
-        frameCount = 0;
-        const step = nextStep();
-        if (step) {
-            addPathStep(forceGraph, ...step);
-        }
-    }
-
-
-}
-
-function splitStep(step){
-    var nodeid = step.slice(0, -1);
-    if (!nodeid.startsWith('s')) {
-        nodeid = 's' + nodeid;
-    }
-    const direction = step.slice(-1);
-    return [nodeid, direction];
-}
-
-export function nextStep(){
-    if (!animationPath) return;
+   if (!animationPath) return;
 
     const tick = tickAnimation();
 
     //reset state
     if (tick == null){ 
         currentStep = -1;
+        stepStructure = [0];
         updateStepDisplay(null);
         return;
     }
@@ -73,15 +66,23 @@ export function nextStep(){
     } 
     // start of animation
     if (tick < 0 && currentStep <= 0) {
+        currentStep = 0;
+        stepStructure = [0];
         pauseAnimation();
         return;
     }
 
     currentStep += tick;
 
-    console.log("Current step:", currentStep, animationPath[currentStep-1]);
-    
-
-
-    return null;//splitStep(animationPath[currentStep]);
+    updatePathStep(tick);
 }
+
+function splitStep(step){
+    var nodeid = step.slice(0, -1);
+    if (!nodeid.startsWith('s')) {
+        nodeid = 's' + nodeid;
+    }
+    const direction = step.slice(-1);
+    return [nodeid, direction];
+}
+
